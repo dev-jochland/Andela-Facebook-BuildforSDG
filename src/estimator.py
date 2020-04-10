00bacb11440227b383e8ad11ecf4865d3c816be0
Joel Onojason <joel.onojason@outlook.com>
2020-04-10 03:10:11 +0100

    Challenge 1 Updated

--- src/estimator.py
+++ src/estimator.py
@@ -37,64 +37,16 @@ def estimator(data):
                                           calculate_days_factor(
                                               data.get("timeToElapse"))))
 
-    # impact["severeCasesByRequestedTime"] = (math.floor(
-    #                                         impact.get
-    #                                         ("infectionByRequestedTime") *
-    #                                         0.15))
-
-    # impact["hospitalBedsRequestedTime"] = (calculate_35_percent_bed(
-    #                                        data.get("totalHospitalBeds")) -
-    #                                        impact.get(
-    #                                            "severeCasesByRequestedTime"))
-
-    # impact["casesForICUByRequestedTime"] = (math.floor(impact.get
-    #                                         ("infectionByRequestedTime")
-    #                                         * 0.05))
-
-    # impact["casesForVentilatorsByRequestedTime"] = (math.floor(
-    #                                                 impact.get(
-    #                                                  "infectionByRequestedTime"
-    #                                                  ) * 0.02))
-
-    # impact["dollarsInFlight"] = (impact.get("infectionByRequestedTime")
-    #                              * 0.71 * 5 * data.get("timeToElapse"))
-
     severeImpact["currentlyInfected"] = data.get("reportedCases") * 50
 
     severeImpact["infectionByRequestedTime"] = (math.floor(severeImpact.get(
         "currentlyInfected") * calculate_days_factor(
             data.get("timeToElapse"))))
 
-    # severeImpact["severeCasesByRequestedTime"] = (math.floor(
-    #     severeImpact.get("infectionByRequestedTime") * 0.15))
-
-    # severeImpact["hospitalBedsRequestedTime"] = (calculate_35_percent_bed(
-    #     data.get("totalHospitalBeds")) - severeImpact.get
-    #     ("severeCasesByRequestedTime"))
-
-    # severeImpact["casesForICUByRequestedTime"] = (math.floor(severeImpact.get(
-    #     "infectionByRequestedTime") * 0.05))
-
-    # severeImpact["casesForVentilatorsByRequestedTime"] = (math.floor(
-    #     severeImpact.get("infectionByRequestedTime") * 0.02))
-
-    # severeImpact["dollarsInFlight"] = (severeImpact.get(
-    #     "infectionByRequestedTime") * 0.71 * 5 * data.get("timeToElapse"))
-
     return output_dictionary
 
 
 def main():
-    # period_type = input("Select one of the following period type you \
-    # wish to estimate\nDays:\nWeeks:\nMonths: ").lower()
-
-    # time_to_elapse = eval(input(f"Input the numbers of {period_type} you \
-    # wish to estimate: "))
-
-    # reported_cases = eval(input("Enter the reported cases: "))
-    # population = eval(input("Enter the total number of population: "))
-    # total_hospital_beds = eval(input("Enter total number of hospital bed: "))
-
     period_type = "days"
     time_to_elapse = 58
     reported_cases = 674
